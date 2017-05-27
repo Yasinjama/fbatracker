df6e45be0aae2df08a694f61ebdfe2b24bc6d17f
YasinMo <YasinMo@172.20.20.20>
2017-05-27 15:29:19 -0500

    Complete Crud for UserAccount. You can Create,Update and Delete User

--- rest/src/main/java/com/fbatracker/rest/v1/UserAccountResource.java
+++ rest/src/main/java/com/fbatracker/rest/v1/UserAccountResource.java
@@ -1,11 +1,16 @@
 package com.fbatracker.rest.v1;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -44,6 +49,67 @@ public class UserAccountResource {
 	{
 		LOGGER.trace("getUserAccount username={}",username);
 		UserAccount userAccount = repository.findUserAccountByUsername(username);
+		if(userAccount == null)
+		{
+			throw new WebApplicationException("User account not found",Response.Status.NOT_FOUND);
+		}
 		return userAccount;
+	}
+	
+	@POST
+	@Path("v1/useraccount/")
+	@ApiOperation(value = "Create User account resource. Version 1",response = UserAccount.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "User account created"),
+			@ApiResponse(code = 400, message = "Invalid request")
+	})
+	public UserAccount createUserAccount(UserAccount userAccount)
+	{
+		UserAccount user = repository.findUserAccountByUsername(userAccount.getUsername());
+		
+		if(user != null)
+		{
+			throw new WebApplicationException("User account with username "+userAccount.getUsername()+" exist already",Response.Status.BAD_REQUEST);
+		}
+		
+		return repository.save(userAccount);
+	}
+	
+	@PUT
+	@Path("v1/useraccount")
+	@ApiOperation(value = "Update User account resource. Version 1",response = UserAccount.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "User account updated"),
+			@ApiResponse(code = 404, message = "User account not found")
+	})
+	public UserAccount updateUserAccount(UserAccount userAccount)
+	{
+		UserAccount user = repository.findUserAccountByUsername(userAccount.getUsername());
+		if(user == null)
+		{
+			throw new WebApplicationException("User account not found",Response.Status.NOT_FOUND);
+		}
+		userAccount.setId(user.getId());
+		return repository.save(userAccount);	
+	}
+	
+	@DELETE
+	@Path("v1/useraccount/{username}")
+	@ApiOperation(value = "Delete User account resource. Version 1",response = UserAccount.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "User account deleted"),
+			@ApiResponse(code = 404, message = "User account not found")
+	})
+	public Response deleteUserAccount(@ApiParam @PathParam("username")String username)
+	{
+		UserAccount user = repository.findUserAccountByUsername(username);
+		if(user == null)
+		{
+			throw new WebApplicationException("User account not found",Response.Status.NOT_FOUND);
+		}
+		
+		repository.delete(user);
+		
+		return Response.ok().build();
 	}
 }
